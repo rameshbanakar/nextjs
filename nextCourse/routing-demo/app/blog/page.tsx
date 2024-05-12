0b7ca109d2dacce4fccb7b49ab7ec00a1190d67e
ramesh banakar <ramesh.s.b@campusuvce.in>
2024-05-12 20:31:17 +0530

    Meta data title changing

--- nextCourse/routing-demo/app/blog/page.tsx
+++ nextCourse/routing-demo/app/blog/page.tsx
@@ -1,5 +1,9 @@
-
-
+import { Metadata } from "next"
+export const metadata:Metadata={
+  title:{
+    absolute:"blog"
+  }
+}
 export default function Blog() {
   return (
       <div>Blog page</div>
--- nextCourse/routing-demo/app/product/[productId]/page.tsx
+++ nextCourse/routing-demo/app/product/[productId]/page.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import { metadata } from "@/app/layout"
+import { Metadata } from "next"
+type props = {
+    params: {
+        productId: String
+    }
 
-export default function ProductDetails({params}:{params:{productId:string}}) {
+}
+export const generateMetadata = async ({ params }: props): Promise<Metadata> => {
+    const titile = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iphone ${params.productId}`)
+        }, 100)
+    })
+    return {
+        title: `product ${titile}`
+    }
+}
+export default function ProductDetails({ params }: props) {
     return (
         <div>product details page {params.productId}</div>
     )
