b4afce093d05cbdfe29e66c7a11bd17a919acd9f
ramesh banakar <ramesh.s.b@campusuvce.in>
2024-05-12 21:25:58 +0530

    active link design

--- nextCourse/routing-demo/app/auth/(with-auth-layout)/layout.tsx
+++ nextCourse/routing-demo/app/auth/(with-auth-layout)/layout.tsx
@@ -1,13 +1,24 @@
+"use client"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+const navLink = [{ name: "register", link: "/auth/register" }, { name: "login", link: "/auth/login" }]
 export default function RootLayout({
+    
     children,
 }: {
     children: React.ReactNode
 }) {
+    const pathname = usePathname()
     return (
-        <>
-            {children}
-            <h6> inner layout</h6>
-        </>
+        <div>
+        {children}
+        {navLink.map((link)=>{
+            const isActive=pathname.startsWith(link.link)
+           return(
+            <Link href={link.link} className={isActive?"font-bold mr-4":"text-blue-500 mr-4"}>{link.name}</Link>
+           )
+        })}
+        </div>
        
     )
 }
